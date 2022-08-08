9a80a69c4822834a16099d9661e64523a3805070
stephenkinyua <kinyuamunene.km@gmail.com>
2022-08-08 10:39:31 +0300

    working with multiple php files

---
+++ including_files/simple_website/index.php
@@ -0,0 +1,4 @@
+<?php include_once 'partials/header.php'; ?>
+<h3>Nairobi, Kenya</h3>
+<h1>Welcome to my cool website</h1>
+<?php include_once 'partials/footer.php'; ?>
---
+++ including_files/simple_website/partials/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    Copyright &copy; 2022
+</footer>
+</body>
+</html>
